c1e1799a87156fcb27810dd204226273cae67878
justforlxz <justforlxz@gmail.com>
2021-02-26 15:42:44 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ try {
     process.exit(-1);
 }
 
-const octokit = new Octokit({ auth: `${token}`})
+const octokit = new Octokit({ auth: `${token}` })
 
 interface Result {
     name: string;
@@ -48,34 +48,93 @@ type Results = Result[];
 
 let result: Results = [];
 
-async function issue(repo: string): Promise<void> {
-    let index: number = 0;
-    let issues: Issues = [];
+async function pull(repo: string): Promise<void> {
+    let index: number = 1;
+    let pulls: Pulls = [];
+    let pullIndex: Array<number> = [];
     for (; ;) {
-        const response = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+        const response = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
             owner: 'linuxdeepin',
             repo,
             page: index,
-            state: 'all'
+            state: 'all',
         });
 
         if (response.status !== 200) {
             continue;
         }
 
-        const tmp: Issues = response.data;
+        let tmpNeed: Pulls = [];
+        response.data.forEach(pull => {
+            let p: IPull = pull;
+            if (pullIndex.indexOf(pull.number) == -1) {
+                pullIndex.push(pull.number);
+                if (pull.merged_at !== null) {
+                    p.state = "merge";
+                }
+                tmpNeed.push(p);
+            }
+        });
 
-        if (tmp.length === 0) {
+        if (tmpNeed.length === 0) {
             break;
         }
 
-        issues = issues.concat(tmp);
+        pulls = pulls.concat(tmpNeed);
         index += 1;
     }
 
-    for (let item of result) {
-        if (item.name == repo) {
-            item.issues = issues;
+    for (let r of result) {
+        if (r.name == repo) {
+            r.pulls = pulls;
+            return;
+        }
+    }
+
+    let s: Result = {
+        name: repo,
+        issues: [],
+        pulls
+    }
+
+    result.push(s);
+}
+
+async function issue(repo: string): Promise<void> {
+    let index: number = 1;
+    let issues: Issues = [];
+    let issueIndex: Array<number> = [];
+    for (; ;) {
+        const response = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+            owner: 'linuxdeepin',
+            repo,
+            page: index,
+            state: 'all',
+        });
+
+        if (response.status !== 200) {
+            continue;
+        }
+
+        let tmpNeed: Issues = [];
+        response.data.forEach(issue => {
+            if (!('pull_request' in issue) && issueIndex.indexOf(issue.number) == -1) {
+                issueIndex.push(issue.number);
+                tmpNeed.push(issue);
+            }
+        });
+
+        if (tmpNeed.length === 0) {
+            break;
+        }
+
+        issues = issues.concat(tmpNeed);
+        index += 1;
+    }
+
+    for (let r of result) {
+        if (r.name == repo) {
+            r.issues = issues;
             return;
         }
     }
@@ -119,19 +178,35 @@ async function repo() {
 
 (async () => {
     await repo();
-})()
+    console.log(`repo,open_issues,closed_issues,open_pulls,closed_pulls,merge_pulls`)
+    result.forEach(r => {
+        let issue_open_count = 0;
+        let issue_close_count = 0;
+        let pull_open_count = 0;
+        let pull_close_count = 0;
+        let pull_merge_count = 0;
 
-console.log(`repo,open,close`)
-for (let item of result) {
-    let opens = 0;
-    let closes = 0;
-    for (let issue of item.issues) {
-        if (issue.state === 'oepn') {
-            opens += 1;
-        }
-        else {
-            closes += 1;
-        }
-    }
-    console.log(`${item.name},${opens},${closes}`)
-}
+        r.issues.forEach(issue => {
+            if (issue.state === "open") {
+                issue_open_count += 1;
+            }
+            else {
+                issue_close_count += 1;
+            }
+        });
+
+        r.pulls.forEach(pull => {
+            if (pull.state === "open") {
+                pull_open_count += 1;
+            }
+            else if (pull.state === "closed") {
+                pull_close_count += 1;
+            }
+            else {
+                pull_merge_count += 1;
+            }
+        });
+
+        console.log(`${repo},${issue_open_count},${issue_close_count},${pull_open_count},${pull_close_count},${pull_merge_count}`);
+    });
+})()
